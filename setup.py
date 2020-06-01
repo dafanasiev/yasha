b4488779647db344d98ee9754022f44e40109e1f
Dmitry Afanasiev <afanasiev.dmitry@gmail.com>
2020-06-01 09:42:48 +0300

    fix version

--- setup.py
+++ setup.py
@@ -21,6 +21,15 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+import re
+import ast
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('yasha/yasha.py', 'rb') as f:
+    __version__ = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
@@ -43,7 +52,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.0",  # Required
+    version=__version__,  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
